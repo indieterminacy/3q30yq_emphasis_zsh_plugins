9b63b62989f73af21f58826d88eab4d421bd6a68
indieterminacy <indieterminacy@nortonspeal.com>
2016-10-07 14:05:31 +0200

    Starting zsh plugin config for emphasis

---
+++ yq_emphasis.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+# Plugin highlights file content based on the filename extension.
+# Did not load or perhaps already loaded
+# zplug "plugins/colorize", \
+#     # from:oh-my-zsh
+#     from:oh-my-zsh, \
+#     if:"which pygmentize"
